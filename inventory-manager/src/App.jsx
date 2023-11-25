68f2aca64288d3e69848da49906a1a6e3f96d8ee
Sarthak Shrivastava <sarthaks@vt.edu>
2023-11-25 00:16:57 -0500

    mostly working authentication + user updating code

--- inventory-manager/src/App.jsx
+++ inventory-manager/src/App.jsx
@@ -11,26 +11,29 @@ import {
 import { useState } from "react";
 import useToken from "./components/useToken";
 import AccountInformation from "./components/user/AccountInformation";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 
 function App() {
   // const [token, setToken] = useState();
   const { token, setToken } = useToken();
   // const token = getToken();
-  if (!token) 
-  {
-    return <Login setToken={setToken}/>
-  }
+  // if (!token) 
+  // {
+  //   return <Login setToken={setToken}/>
+  // }
   return (
     <div className="App">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login setToken={token}/>} />
+        <Route path="/login" element={<Login setToken={setToken}/>} />
         <Route path="/register" element={<Register />} />
-        <Route path="/deleteUser" element={<DeleteUser token={token}/>} />
+        <Route path='/accountinfo' element={<ProtectedRoute token={token}> <AccountInformation token={token}/> </ProtectedRoute>}>
+        </Route>
+        {/* <Route path="/deleteUser" element={<DeleteUser token={token}/>} />
         <Route path="/updatepassword" element={<UpdatePassword />} />
-        <Route path="/accountinfo" element={<AccountInformation token={token}/> } />
+        <Route path="/accountinfo" element={<AccountInformation token={token}/> } /> */}
       </Routes>
     </div>
   );
